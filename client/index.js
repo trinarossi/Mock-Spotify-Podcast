cb0b7fa5d00f608e6e3cd0bb269b0a1e54cd28b0
Katrina <katrossi414@gmail.com>
2023-02-23 13:51:37 -0600

    Initial folders created. All programs downloaded. index.html created and root connected.

---
+++ client/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Main } from "./components";
+import { Provider } from "react-redux";
+import store from "./store";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+        <Main />
+    </BrowserRouter>
+  </Provider>
+);
